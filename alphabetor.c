f5b865058b99b3fa82fc79fae1bc95bffea73af9
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-01-22 16:29:07 +0530

    Update alphabetor.c

---
+++ alphabetor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+int main()
+{  
+ char character
+ printf("Enter a Character\n");  
+ scanf("%c", &character);  
+ if((character >= 'a' && character <= 'z')l;
+ {
+ printf("%c is an Alphabet\n", character);  
+ } 
+    else
+ {  
+ printf("%c is Not an Alphabet\n", character);  
+ }
+ return 0;  
+}
